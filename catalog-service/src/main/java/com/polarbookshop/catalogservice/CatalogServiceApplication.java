9ac32099f9a0458e8c474d777f9342f06fec3f08
EreMin <barold.eremin@gmail.com>
2024-09-17 09:53:13 +0900

    more study yay

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -3,7 +3,7 @@ package com.polarbookshop.catalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Includes @Configuration / @ComponentScan / @EnabledAutoConfiguration
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
