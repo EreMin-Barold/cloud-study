1f39071ee634e699f0476b9bdf06cebc2ce03a38
EreMin <barold.eremin@gmail.com>
2024-09-21 13:47:02 +0900

    add mockbean

--- catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,14 +1,17 @@
 package com.polarbookshop.catalogservice;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
@@ -17,9 +20,14 @@ class CatalogServiceApplicationTests {
 	@Autowired
 	private WebTestClient webTestClient;
 	
+	@MockBean
+	private BookRepository bookRepository;
+	
 	@Test
 	void whenPostRequestThenBookCreated() {
 		var expectedBook = Book.of("1231231231", "Title", "Author",9.90, "publisher");
+		
+		when(bookRepository.save(expectedBook)).thenReturn(expectedBook);
 		
 		webTestClient
 			.post()
